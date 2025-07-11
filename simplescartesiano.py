460d69df9fa23d3d1a47cc4344ab4f1fc32091ac
Davi <davimaciel2008@gmail.com>
2025-07-11 12:02:49 -0300

    Roots no gráfico e executável portátil

--- simplescartesiano.py
+++ simplescartesiano.py
@@ -25,6 +25,17 @@ def PegarCoeficientes(name):
             print(f"O coeficiente {name} deve ser um número inteiro!")
 
 
+def PlotarRaizes(roots, a):    # Função para mostrar o momento em que o gráfico toca o eixo X (y == 0)
+    for i, x in enumerate(roots):   # Enumera a quantidade de raízes para alinhar corretamente o texto
+        plt.scatter(x, 0, color="darkblue", zorder=5)  #Função da plt que gera pontos individuais no gráfico. No caso, para todas as roots (raízes da equação).
+        # i == 0 é a primeira raíz, i == 1 é a segunda
+        if (a > 0 and i == 0) or (a < 0 and i == 1):    # Check se a parábola é crescente ou decrescente, pois "xOne" é o valor a esquerda na parábola crescente e vice-versa.
+            ha = "left" # Logo, sempre que a parábola é crescente, a raíz mais a esquerda é a 0. Então o melhor alinhamento é a esquerda do texto. O mesmo se aplica pro oposto
+        else:
+            ha = "right"    # Para a outra raíz, o melhor é alinhar na direita (legibilidade).
+        plt.text(x, 0, f"x = {round(x, 2)}", fontsize="medium", ha=ha, va='bottom', color="darkblue")
+    
+    
 # Uma função só, pela simplicidade da constante
 def ConstanteC():
     print("A e B são iguais a 0. O gráfico é constante, onde Y = C.")
@@ -71,6 +82,7 @@ def PrimeiroGrau():
     plt.ylim(np.min(yValues), np.max(yValues))  # Margem de 0 para todos os pontos serem mostrados.
         
     plt.plot(xValues, yValues, label=rf"$y = {b}x + {c}$", color="red", linewidth=2)
+    PlotarRaizes([x], a)
     plt.title("Gráfico da Equação do Primeiro Grau")
     plt.xlabel("Eixo X")
     plt.ylabel("Eixo Y")
@@ -90,17 +102,19 @@ def SegundoGrau():
         print("Essa equação possui duas raízes reais e distintas!")
         print(f"O valor de X₁ é: {round(xOne, 2)}.")
         print(f"O valor de X₂ é: {round(xTwo, 2)}.")
+        return [xOne, xTwo]
     elif valDelta == 0:
         xOne = (-b + valDelta ** 0.5) / (2 * a) 
         print("Essa equação possui duas raízes reais e iguais!")
-        print(f"O valor de X₁ é: {round(xOne, 2)}.")  
+        print(f"O valor de X₁ é: {round(xOne, 2)}.")
+        return [xOne]  
     else:
-        print("Essa equação não possui raízes reais.")
-        return valDelta    
-    
+        print("Essa equação não possui raízes reais.")  
+
                     
 def GrafSegundoGrau():
-    width = 10 if abs(a) > 1 or abs(a) == 0 else int(15 / abs(a))    # Se o valor de A for baixo, o gráfico será maior (maior amplitude).
+    roots = SegundoGrau()
+    width = 10 if abs(a) > 1 else int(15 / abs(a))    # Se o valor de A for baixo, o gráfico será maior (maior amplitude).
     xVer = (-b) / (2 * a) if b != 0 else 0
     # xValues = range(int((xVer - 5)), int((xVer + 5)))   # Determina os possíveis valores de X da equação  
     xValues = np.linspace(int(xVer - width), int(xVer + width), 500)  # Determina os possíveis valores de X da equação
@@ -114,6 +128,12 @@ def GrafSegundoGrau():
     plt.ylim(np.min(yValues) - 5, np.max(yValues) + 5)  # Margem de 5 para o limite do eixo Y ficar mais dinâmico
         
     plt.plot(xValues, yValues, label=rf"$y = {a}x² + {b}x + {c}$", color="red", linewidth=2)
+    
+    if roots is not None:
+        PlotarRaizes(roots, a)
+    else:
+        None        
+                
     plt.title("Gráfico da Equação do Segundo Grau")
     plt.xlabel("Eixo X")
     plt.ylabel("Eixo Y")
@@ -122,7 +142,7 @@ def GrafSegundoGrau():
     plt.show()
  
     
-num = input("Calcule uma equação do 2° grau! Aperte N para sair ou qualquer tecla para iniciar. ").upper()
+num = input("Calcule uma equação do 1° ou do 2° grau! Aperte N para sair ou qualquer tecla para iniciar. ").upper()
 
 while not "N" in num:
     a, b, c = [PegarCoeficientes(v) for v in ["A", "B", "C"]] # Pega os valores de A, B e C com line comprehension.
@@ -131,7 +151,6 @@ while not "N" in num:
     elif (a == 0) and (b != 0):
         PrimeiroGrau()
     else:    
-        SegundoGrau()
         GrafSegundoGrau()
     
     num = input("Aperte N para sair ou qualquer tecla para reiniciar. ").upper()
